1d870a79859896012dae8711f3bf0aeafa3c82e3
zahrahaji10 <102627226+zahrahaji10@users.noreply.github.com>
2022-05-08 14:49:24 +0100

    adding html into js dynamically

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,6 +3,106 @@
 // function to create HTML for each time block, append to page
 const renderTimeBlocks = () => {
   // target the time block container
+  $("#time-block-container")
+    .append(`<div class="time-blocks d-flex flex-row align-items-center">
+  <div class="time-label">09:00am</div>
+  <textarea
+    class="text-area form-control"
+    id="floatingTextarea2"
+    style="height: 100px"
+  ></textarea>
+  <div class="button-container">
+    <button class="save-button">save</button>
+  </div>
+</div>
+<div class="time-blocks d-flex flex-row align-items-center">
+  <div class="time-label">10:00am</div>
+  <textarea
+    class="text-area form-control"
+    id="floatingTextarea2"
+    style="height: 100px"
+  ></textarea>
+  <div class="button-container">
+    <button class="save-button">save</button>
+  </div>
+</div>
+<div class="time-blocks d-flex flex-row align-items-center">
+  <div class="time-label">11:00am</div>
+  <textarea
+    class="text-area form-control"
+    id="floatingTextarea2"
+    style="height: 100px"
+  ></textarea>
+  <div class="button-container">
+    <button class="save-button">save</button>
+  </div>
+</div>
+<div class="time-blocks d-flex flex-row align-items-center">
+  <div class="time-label">12:00pm</div>
+  <textarea
+    class="text-area form-control"
+    id="floatingTextarea2"
+    style="height: 100px"
+  ></textarea>
+  <div class="button-container">
+    <button class="save-button">save</button>
+  </div>
+</div>
+<div class="time-blocks d-flex flex-row align-items-center">
+  <div class="time-label">13:00pm</div>
+  <textarea
+    class="text-area form-control"
+    id="floatingTextarea2"
+    style="height: 100px"
+  ></textarea>
+  <div class="button-container">
+    <button class="save-button">save</button>
+  </div>
+</div>
+<div class="time-blocks d-flex flex-row align-items-center">
+  <div class="time-label">14:00pm</div>
+  <textarea
+    class="text-area form-control"
+    id="floatingTextarea2"
+    style="height: 100px"
+  ></textarea>
+  <div class="button-container">
+    <button class="save-button">save</button>
+  </div>
+</div>
+<div class="time-blocks d-flex flex-row align-items-center">
+  <div class="time-label">15:00pm</div>
+  <textarea
+    class="text-area form-control"
+    id="floatingTextarea2"
+    style="height: 100px"
+  ></textarea>
+  <div class="button-container">
+    <button class="save-button">save</button>
+  </div>
+</div>
+<div class="time-blocks d-flex flex-row align-items-center">
+  <div class="time-label">16:00pm</div>
+  <textarea
+    class="text-area form-control"
+    id="floatingTextarea2"
+    style="height: 100px"
+  ></textarea>
+  <div class="button-container">
+    <button class="save-button">save</button>
+  </div>
+</div>
+<div class="time-blocks d-flex flex-row align-items-center">
+  <div class="time-label">17:00pm</div>
+  <textarea
+    class="text-area form-control"
+    id="floatingTextarea2"
+    style="height: 100px"
+  ></textarea>
+  <div class="button-container">
+    <button class="save-button">save</button>
+  </div>
+</div>`);
 };
 
 // function to target the current date section
