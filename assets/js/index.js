38eef9bd96426c6cf9b901264a7d621d03ce90d6
zahrahaji10 <102627226+zahrahaji10@users.noreply.github.com>
2022-05-08 10:17:23 +0100

    using moment js for rendering time

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,14 +1,24 @@
+//~ FUNCTIONS SECTION
+
+// function to create HTML for each time block, append to page
+const renderTimeBlocks = () => {};
+
 // function to target the current date section
 const renderDate = () => {
-  // get current date from moment js
-  //format that date
+  // get current date from moment js and format date/time
+  let dateAndTime = moment().format("dddd, MMMM Do, YYYY -- hh:mm A");
   //set the text.content to in the <p> to update the date
+  $("dateAndTime").append(currentDay);
 };
 
-// function when page loads
+// First function to execute when page loads
 const onReady = () => {
-  console.log("page loaded");
+  // first action on this function is to call renderDate fn
+  renderDate();
+  // then it calls the renderTimeBlock fn
+  renderTimeBlocks();
 };
 
+//? EVENT LISTENER
 //event lister for whole document when it loads
-pageOnLoad.addEventListener("click", onReady);
+$(window).on("load", onReady);
